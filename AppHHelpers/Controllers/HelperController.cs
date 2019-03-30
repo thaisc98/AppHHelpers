4a23085e46d9f34a2e42d18f8df8aa7af7ff9d88
Tнαιѕ Cσηтяєяαѕ <thaisc98@hotmail.es>
2019-03-30 09:15:37 -0500

    Base de Datos + Create();

--- AppHHelpers/Controllers/HelperController.cs
+++ AppHHelpers/Controllers/HelperController.cs
@@ -3,16 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AppHHelpers.Models;
 
 namespace AppHHelpers.Controllers
 {
     public class HelperController : Controller
     {
+        private HtmlHelpDbEntities db;
+
+        public HelperController()
+        {
+            db = new HtmlHelpDbEntities();
+        }
 
         // GET: Helper
         public ActionResult Index()
         {
             return View();
         }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.GeneroId = new SelectList(db.Genero.ToList(),"Id", "Descripcion");
+
+            List<string> list = new List<string>();
+            
+            list.Add("Ver pelicula");
+            list.Add("Leer manga");
+            list.Add("Leer");
+            list.Add("Estudiar");
+
+
+            ViewBag.Hobbies = list;
+            return View();
+        }
+
+        
+
+
+
+
+
+
     }
 }
